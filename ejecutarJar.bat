ab80bed955e8786383355fe3bc2d93b06018c951
JonmuxuPvP <jgarciatdaw1@gmail.com>
2021-05-20 21:57:47 +0200

    Subida de Jar

--- ejecutarJar.bat
+++ ejecutarJar.bat
@@ -5,6 +5,6 @@ IF  "%1%" == "" (
   @ECHO  teclee nombre de fichero jar 
 )  else  (
 	REM  java --module-path  "C:\Java\javafx11\lib"  --add-modules=javafx.controls -jar %1
-	java --module-path  %PATH_TO_FX% --add-modules=javafx.controls,javafx.fxml -jar %1
+	java --module-path "C:\Program Files\Java\javafx11\lib" --add-modules=javafx.controls,javafx.fxml -jar %1
 )
 
